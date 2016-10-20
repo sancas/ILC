99729889710baed699366c597aea9a71b5513e10
Cristian Sanchez <sancas19@gmail.com>
2016-10-19 18:32:28 -0600

    Se creo el Login

--- Proyecto/Proyecto/Login.Designer.cs
+++ Proyecto/Proyecto/Login.Designer.cs
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.LoginStyleManager = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.txtEmail = new MetroFramework.Controls.MetroTextBox();
+            this.txtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.btnAceptar = new MetroFramework.Controls.MetroTile();
+            this.btnCancelar = new MetroFramework.Controls.MetroTile();
+            ((System.ComponentModel.ISupportInitialize)(this.LoginStyleManager)).BeginInit();
             this.SuspendLayout();
+            // 
+            // LoginStyleManager
+            // 
+            this.LoginStyleManager.Owner = this;
+            // 
+            // txtEmail
+            // 
+            // 
+            // 
+            // 
+            this.txtEmail.CustomButton.Image = null;
+            this.txtEmail.CustomButton.Location = new System.Drawing.Point(222, 2);
+            this.txtEmail.CustomButton.Name = "";
+            this.txtEmail.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.txtEmail.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtEmail.CustomButton.TabIndex = 1;
+            this.txtEmail.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtEmail.CustomButton.UseSelectable = true;
+            this.txtEmail.CustomButton.Visible = false;
+            this.txtEmail.DisplayIcon = true;
+            this.txtEmail.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtEmail.Icon = ((System.Drawing.Image)(resources.GetObject("txtEmail.Icon")));
+            this.txtEmail.IconRight = true;
+            this.txtEmail.Lines = new string[0];
+            this.txtEmail.Location = new System.Drawing.Point(75, 89);
+            this.txtEmail.MaxLength = 32767;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.PasswordChar = '\0';
+            this.txtEmail.PromptText = "Email";
+            this.txtEmail.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtEmail.SelectedText = "";
+            this.txtEmail.SelectionLength = 0;
+            this.txtEmail.SelectionStart = 0;
+            this.txtEmail.ShortcutsEnabled = true;
+            this.txtEmail.Size = new System.Drawing.Size(250, 30);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.UseSelectable = true;
+            this.txtEmail.UseStyleColors = true;
+            this.txtEmail.WaterMark = "Email";
+            this.txtEmail.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtEmail.WaterMarkFont = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtPassword
+            // 
+            // 
+            // 
+            // 
+            this.txtPassword.CustomButton.Image = null;
+            this.txtPassword.CustomButton.Location = new System.Drawing.Point(222, 2);
+            this.txtPassword.CustomButton.Name = "";
+            this.txtPassword.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.txtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtPassword.CustomButton.TabIndex = 1;
+            this.txtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtPassword.CustomButton.UseSelectable = true;
+            this.txtPassword.CustomButton.Visible = false;
+            this.txtPassword.DisplayIcon = true;
+            this.txtPassword.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtPassword.Icon = ((System.Drawing.Image)(resources.GetObject("txtPassword.Icon")));
+            this.txtPassword.IconRight = true;
+            this.txtPassword.Lines = new string[0];
+            this.txtPassword.Location = new System.Drawing.Point(75, 147);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.txtPassword.MaxLength = 32767;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.PromptText = "Password";
+            this.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.SelectionLength = 0;
+            this.txtPassword.SelectionStart = 0;
+            this.txtPassword.ShortcutsEnabled = true;
+            this.txtPassword.Size = new System.Drawing.Size(250, 30);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSelectable = true;
+            this.txtPassword.UseStyleColors = true;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.WaterMark = "Password";
+            this.txtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.ActiveControl = null;
+            this.btnAceptar.Location = new System.Drawing.Point(57, 213);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(131, 48);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.TileImage = ((System.Drawing.Image)(resources.GetObject("btnAceptar.TileImage")));
+            this.btnAceptar.TileImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.btnAceptar.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.btnAceptar.UseSelectable = true;
+            this.btnAceptar.UseStyleColors = true;
+            this.btnAceptar.UseTileImage = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.ActiveControl = null;
+            this.btnCancelar.Location = new System.Drawing.Point(263, 213);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(131, 48);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.TileImage = ((System.Drawing.Image)(resources.GetObject("btnCancelar.TileImage")));
+            this.btnCancelar.TileImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.btnCancelar.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.btnCancelar.UseSelectable = true;
+            this.btnCancelar.UseStyleColors = true;
+            this.btnCancelar.UseTileImage = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(450, 300);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
             this.Name = "Login";
+            this.Resizable = false;
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.LoginStyleManager)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Components.MetroStyleManager LoginStyleManager;
+        private MetroFramework.Controls.MetroTile btnAceptar;
+        private MetroFramework.Controls.MetroTextBox txtPassword;
+        private MetroFramework.Controls.MetroTextBox txtEmail;
+        private MetroFramework.Controls.MetroTile btnCancelar;
     }
 }
 
